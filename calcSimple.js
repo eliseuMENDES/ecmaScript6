f2e9b3d4b6c479267bf80243012cc426f996388a
eliseuMendes <eliseu839@gmail.com>
2025-04-01 00:33:21 -0300

    Initial commit with project structure

---
+++ calcSimple.js
@@ -0,0 +1,33 @@
+// // function calculadora(num1, num2, operacao) {
+// //     num1 = 15 + 37
+// //     num2 = 18 - 12
+// //     operacao = num1 * num2
+    
+// //     console.log(operacao);
+// // }
+
+// // console.log(calculadora());
+
+
+// Calculadora correta :
+
+// function calculadora(num1, num2, operacao){
+//     switch(operacao) {
+//         case '+':
+//             return num1 + num2;
+//         case '-':
+//             return num1 - num2;
+//         case '*':
+//             return num1 * num2;
+//         case '/':
+//             return num1 / num2;
+//         default:
+//             return "Operação Inválida";
+//     }
+// }
+
+// console.log(calculadora(10, 5, '+')); // 15
+// console.log(calculadora(10, 5, '-')); // 5
+// console.log(calculadora(10, 5, '*')); // 50
+// console.log(calculadora(10, 5, '/')); // 2
+// console.log(calculadora(10, 5, 'a')); // Operação Inválida.
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const chokidar = require('chokidar');
+
+const app = express();
+const PORT = 3000;
+
+//Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+//Rota para obter dados de estudos
+app.get('/api/estudos', (req, res) => {
+    fs.readFile('./data/estudos.json', 'utf8', (err, data) =>{
+        if (err) return res.status(500).send(err);
+        res.json(JSON.parse(data));
+    });
+});
+
+//Live reload
+const watcher = chokidar.watch(['./data', './public']);
+watcher.on('change', () => {
+    console.log('Arquivos modificados - recarregando...');
+    // Aqui você pode implementar WebSocket para atualizar o cliente
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log('Monitorando alterações nos  arquivos...');
+})
